f7bb536db61c54c1234a672da4267c45704b0688
utkarsh059 <utkarsh05sep05@gmail.com>
2026-06-02 20:16:08 +0530

    Added Day2 Assignment

---
+++ Day2/Q1_SumOfDigits.java
@@ -0,0 +1,23 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Q1_SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        while (num != 0) {
+            sum += num % 10; 
+            num /= 10;       
+        }
+
+        System.out.println("Sum of digits = " + sum);
+
+        sc.close();
+    }
+}
---
+++ Day2/Q2_ReverseNumber.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class Q2_ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;       // get last digit
+            reverse = reverse * 10 + digit;
+            num = num / 10;             // remove last digit
+        }
+
+        System.out.println("Reversed number = " + reverse);
+
+        sc.close();
+    }
+}
+    
+
