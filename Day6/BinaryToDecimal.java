1b473361008c5c624ce0edf044ab905473e57ca9
utkarsh059 <utkarsh05sep05@gmail.com>
2026-06-06 19:13:05 +0530

    day 6 completed

---
+++ Day6/BinaryToDecimal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a binary number: ");
+        int binary = sc.nextInt();
+
+        int decimal = 0;
+        int base = 1; // 2^0
+
+        while (binary > 0) {
+            int lastDigit = binary % 10;
+            decimal += lastDigit * base;
+            base *= 2;
+            binary /= 10;
+        }
+
+        System.out.println("Decimal = " + decimal);
+    }
+}
---
+++ Day6/CountSetBits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CountSetBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        while (num > 0) {
+            if ((num & 1) == 1) {
+                count++;
+            }
+            num = num >> 1;
+        }
+
+        System.out.println("Number of set bits = " + count);
+    }
+}
